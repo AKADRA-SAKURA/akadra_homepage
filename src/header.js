00b74a9edf4241353c6ded2d9b73cf277be70627
AKADRA <akadra.sakura7@gmail.com>
2023-01-21 13:05:40 +0900

    header実装

--- src/header.js
+++ src/header.js
@@ -1,17 +1,22 @@
 import React from 'react';              //Reactを読み込んでいる
 import { Link } from 'react-router-dom';
+import './header.css';
 
 class header extends React.Component {   //page1クラスにReact.Componentを継承する
     render() {                          //画面表示の為のrenderメソッドを定義する
         return (
-            <div>
-                <Link to={`/`}>AKADRA Archives</Link>
-                <Link to={`/about`}>about</Link>
-                <Link to={`/web`}>web</Link>
-                <Link to={`/game`}>Game</Link>
-                <Link to={`/dtm`}>DTM</Link>
-                <Link to={`/graphic`}>Graphic</Link>
-                <Link to={`/other`}>other</Link>
+            <div class="nav_all">
+                <div class="title">
+                    <Link to={`/`}>AKADRA's Archives</Link>
+                </div>
+                <div class="menu">
+                    <Link to={`/about`} class="menubox">about</Link>
+                    <Link to={`/web`} class="menubox">web</Link>
+                    <Link to={`/game`} class="menubox">Game</Link>
+                    <Link to={`/dtm`} class="menubox">DTM</Link>
+                    <Link to={`/graphic`} class="menubox">Graphic</Link>
+                    <Link to={`/other`} class="menubox">other</Link>
+                </div>
             </div>
         );
     }
