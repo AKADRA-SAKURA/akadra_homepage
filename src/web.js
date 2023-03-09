19bfd6f447c4d68021befcf22858a7b6b639d5d7
AKADRA <akadra.sakura7@gmail.com>
2023-03-09 23:04:47 +0900

    axios実装

--- src/web.js
+++ src/web.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
+import axios from "axios";
 import { Link } from 'react-router-dom';
 import './background.css';
+import './box_container.css';
+import './web.css';
 
 function web () {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios.get('https://script.google.com/macros/s/AKfycby3qqVTyFrLBo5bVsUxCkm-rOH-mWhSkLTXEjy3PBdUGmRRZU1Dv7_qFwP76zjfuaIgdA/exec')
+        .then(response => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
+
         return (
             <div>
                 web!!!
                 <br/>
-                <Link to="/">Go To home</Link>
             </div>
         );
     }
