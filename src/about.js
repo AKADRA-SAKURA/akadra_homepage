429562330addaf826464bcdb7a8d654c4dba45ab
AKADRA <akadra.sakura7@gmail.com>
2023-03-09 22:55:16 +0900

    ボタン追加

--- src/about.js
+++ src/about.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState }  from 'react';
+import Button from '@mui/material/Button';
 import axios from "axios";
 import './box_container.css';
 import './about.css';
 import './background.css';
+import { createTheme } from '@mui/material/styles';
 import akadra1 from './images/akadra1.jpg'; 
+
+const myTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#e0e0e0",
+      },
+      secondary: {
+        main: "#424242",
+      },
+    },
+  });
 
 function about() {
     const [data, setData] = useState([]);
@@ -24,30 +37,30 @@ function about() {
                     <div className="hidari">
                         <div className="photo box">
                             <img className="profile-picture" src={akadra1} alt="akadra" />
-                            from あろす(@Aros0408)<br/>
-                            <a href="https://github.com/AKADRA-SAKURA">Githubアカウント</a>
+                            from あろす(<a href="https://twitter.com/Aros0408">@Aros0408</a>)<br/>
+                            <Button variant="contained" size="large" theme={myTheme}><a href="https://github.com/AKADRA-SAKURA">Githubアカウント</a></Button>
                         </div>
 
                     </div>
 
                     <div className="migi">
                         <div className="profile box">
-                                <div className="list" >
-                                    <div className="websitebox">
-                                        <div className="detailbox">
+                            <div className="list">
+                                <div className="websitebox">
+                                    <div className="detailbox">
                                         <h2>AKADRA</h2>
-                                        <hr/>
+                                        <hr />
                                         <div className="belongs">
-                                            【所属】{data.map((d) => ( <div>{d.belongs}</div>))}<br />
+                                            【所属】{data.map((d) => (<div>{d.belongs}</div>))}<br />
                                         </div>
-                                        <hr/>
+                                        <hr />
                                         <div className="details">
-                                            【趣味】{data.map((d) => ( <div>{d.hobby}</div>))}<br />
-                                            【好きなもの】{data.map((d) => ( <div>{d.like}</div>))}<br />
-                                            【言語】{data.map((d) => ( <div>{d.language}</div>))}<br />
-                                            【ツール】{data.map((d) => ( <div>{d.cloud}</div>))}<br />
-                                            【プラットフォーム】{data.map((d) => ( <div>{d.platform}</div>))}<br />
-                                            【DTM】 {data.map((d) => ( <div>{d.dtm}</div>))}<br />
+                                            【趣味】{data.map((d) => (<div>{d.hobby}</div>))}<br />
+                                            【好きなもの】{data.map((d) => (<div>{d.like}</div>))}<br />
+                                            【言語】{data.map((d) => (<div>{d.language}</div>))}<br />
+                                            【ツール】{data.map((d) => (<div>{d.cloud}</div>))}<br />
+                                            【プラットフォーム】{data.map((d) => (<div>{d.platform}</div>))}<br />
+                                            【DTM】 {data.map((d) => (<div>{d.dtm}</div>))}<br />
                                         </div>
                                     </div>
                                 </div>
