18e7b631051759b4f13709d701cc4764dbd0db5e
AKADRA <akadra.sakura7@gmail.com>
2023-03-11 00:01:37 +0900

    dtmページ第１案

--- src/dtm.js
+++ src/dtm.js
@@ -1,14 +1,38 @@
-import React from 'react';              // Reactを読み込んでいる
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState }  from 'react';
+import axios from "axios";
 import './background.css';
+import './box_container.css';
+import './dtm.css';
 
-function dtm () {   // page2クラスにReact.Componentを継承する
+
+function dtm () {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios.get('https://script.google.com/macros/s/AKfycbxklHYaKjDs5xo2Yt4wRhf2XW7HD4CbfHH8hL8Pu0brYa-fb2U7QebdrBv-zzqTQ24vGg/exec')
+        .then(response => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
+
         return (
-            <div>
-                dtm!!!
-                <br/>
-                <Link to="/">Go To home</Link>
+            <div className="bg">
+                <div className="dtm_all">
+                    {data.map((d) => (
+                        <div className="box">
+                            <h1>{d.no}：「{d.title}」</h1><hr/>
+                                リリース日：{d.date}<br />
+                                サークル名：{d.circle}<br /> 
+                                アルバム名：{d.album}<br /> 
+                        </div>
+                    ))}
+                </div>
             </div>
         );
-    }
-export default dtm;                   // page2を出力する為に必要
+}
+
+export default dtm;
--- src/game.js
+++ src/game.js
@@ -1,14 +1,57 @@
-import React from 'react';              // Reactを読み込んでいる
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState }  from 'react';
+import Button from '@mui/material/Button';
+import axios from "axios";
 import './background.css';
+import './box_container.css';
+import { createTheme } from '@mui/material/styles';
+import './game.css';
 
-function game () {   // page2クラスにReact.Componentを継承する
+const myTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#e0e0e0",
+      },
+      secondary: {
+        main: "#424242",
+      },
+    },
+  });
+
+function game () {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      axios.get('https://script.google.com/macros/s/AKfycby3qqVTyFrLBo5bVsUxCkm-rOH-mWhSkLTXEjy3PBdUGmRRZU1Dv7_qFwP76zjfuaIgdA/exec')
+        .then(response => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, []);
+
         return (
-            <div>
-                game!!!
-                <br/>
-                <Link to="/">Go To home</Link>
+            <div className="bg">
+                <div className="web_all">
+                    {data.map((d) => (
+                        <div className="box">
+                            <h1>{d.no}：「{d.title}」</h1><hr/>
+                            <div className='hidari'>
+                                <Button variant="contained" size="large" theme={myTheme}><a href={d.url}>URL</a></Button>
+                            </div>
+                            <div className='migi'>
+                                <h3>{d.summary}</h3>
+                                使用言語：{d.language}<br />
+                                チーム人数：{d.people}<br /> 
+                                役割：{d.role}<br /> 
+                                説明：{d.detail}<br /> 
+                                <Button variant="contained" size="large" theme={myTheme}><a href={d.githuburl}>Github URL</a></Button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
 }
-export default game;                   // page2を出力する為に必要
+export default game;
