cc00c4c7c35e265e0fdc0be09496dfb193e3af05
AKADRA <akadra.sakura7@gmail.com>
2023-03-11 10:30:31 +0900

    other,graphicページ第１案

--- src/other.js
+++ src/other.js
@@ -1,15 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState }  from 'react';
+import axios from "axios";
+
 import './background.css';
+import './box_container.css';
+import './other.css';
+import Yt from './youtube';
+
+
 
 function other () {
+    const [dataother, setData1] = useState([]);
+    const [datadance, setData2] = useState([]);
+
+    useEffect(() => {
+    const fetchData = async () => {
+        const result1 = await axios.get("https://script.google.com/macros/s/AKfycbw2sFQTNZbr2Gy0H9GQbAcEFRuJRmWdZHnTixKvS_qpJuaQ8gkqemuv1u9XeRMtHYA7fw/exec");
+        const result2 = await axios.get("https://script.google.com/macros/s/AKfycbwymn9dgm05cVF_irFpYjtyvj5qkCeTaor3zbCV21VCOLJrlqeIhHIseGzQp9I9-w/exec");
+        setData1(result1.data);
+        setData2(result2.data);
+    };
+    fetchData();
+    }, []);
+
         return (
-            <div>
-                other!!!
-                <br/>
-                <Link to="/">Go To home</Link>
+            <div className="bg">
+                <div className="other_all">
+                    {dataother.map((d) => (
+                        <div className="box">
+                            <div className='Movie'>
+                                <h1>{d.no}「{d.title}」</h1><hr/>
+                                <div className='hidari'>
+                                    <a href={d.url}> 
+                                    <Yt /></a>
+                                </div>
+                                <div className='migi'>
+                                    リリース年：{d.release}<br />
+                                    チーム人数：{d.people}<br />
+                                    役割：{d.role}<br /> 
+                                    説明：{d.detail}<br /> 
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="other_all">
+                    <div className="box">
+                        <h2>ダンス経験 from パンドラ</h2><hr/>
+                        {datadance.map((d) => (
+                            <>{d.no}：{d.artist} / {d.title} &#40;{d.detail}&#41;<br/></>
+                        ))}
+                    </div>
+                </div>
             </div>
         );
-    }
-
+}
 export default other;
