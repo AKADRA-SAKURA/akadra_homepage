d5cc6cb2fd59316a12807d202b4737d784c1b728
AKADRA <akadra.sakura7@gmail.com>
2023-07-30 07:55:45 +0900

    ハンバーガーメニューを遷移後に自動で閉じる

--- src/header.js
+++ src/header.js
@@ -2,38 +2,43 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import './header.css';
 
-function header () {   // page1クラスにReact.Componentを継承する
+function Header() {
+  // ハンバーガーメニューを閉じる関数
+  const closeMenu = () => {
+    const drawerInput = document.getElementById('drawer_input');
+    drawerInput.checked = false;
+  };
 
-        return (
-            <div className="nav_all">
-                <div className="title">
-                    <Link to="/" className="titlebox">AKADRA Archives</Link>
-                </div>
-                <div className="menu">
-                    <Link to="/about" className="menubox">about</Link>
-                    <Link to="/web" className="menubox">web</Link>
-                    <Link to="/game" className="menubox">Game</Link>
-                    <Link to="/dtm" className="menubox">DTM</Link>
-                    <Link to="/graphic" className="menubox">Graphic</Link>
-                    <Link to="/other" className="menubox">other</Link>
-                </div>
+  return (
+    <div className="nav_all">
+      <div className="title">
+        <Link to="/" className="titlebox">AKADRA Archives</Link>
+      </div>
+      <div className="menu">
+        <Link to="/about" className="menubox" onClick={closeMenu}>about</Link>
+        <Link to="/web" className="menubox" onClick={closeMenu}>web</Link>
+        <Link to="/game" className="menubox" onClick={closeMenu}>Game</Link>
+        <Link to="/dtm" className="menubox" onClick={closeMenu}>DTM</Link>
+        <Link to="/graphic" className="menubox" onClick={closeMenu}>Graphic</Link>
+        <Link to="/other" className="menubox" onClick={closeMenu}>other</Link>
+      </div>
 
-                <div className="nav hamburger">
-                    <input id="drawer_input" className="drawer_hidden" type="checkbox"></input>               
-                    <label for="drawer_input" className="drawer_open"><span></span></label>               
-                    <nav className="nav_content">
-                    <ul className="nav_list">
-                        <li className="nav_item"><Link to="/about" className="menubox">about</Link></li>
-                        <li className="nav_item"><Link to="/web" className="menubox">web</Link></li>
-                        <li className="nav_item"><Link to="/game" className="menubox">Game</Link></li>
-                        <li className="nav_item"><Link to="/dtm" className="menubox">DTM</Link></li>
-                        <li className="nav_item"><Link to="/graphic" className="menubox">Graphic</Link></li>
-                        <li className="nav_item"><Link to="/other" className="menubox">other</Link></li>
-                    </ul>
-                    </nav>
-            
-                </div>
-            </div>
-        );
-    }
-export default header;  
+      <div className="nav hamburger">
+        <input id="drawer_input" className="drawer_hidden" type="checkbox" />
+        <label htmlFor="drawer_input" className="drawer_open"><span></span></label>
+        <nav className="nav_content">
+          <ul className="nav_list">
+            <li className="nav_item"><Link to="/about" className="menubox" onClick={closeMenu}>about</Link></li>
+            <li className="nav_item"><Link to="/web" className="menubox" onClick={closeMenu}>web</Link></li>
+            <li className="nav_item"><Link to="/game" className="menubox" onClick={closeMenu}>Game</Link></li>
+            <li className="nav_item"><Link to="/dtm" className="menubox" onClick={closeMenu}>DTM</Link></li>
+            <li className="nav_item"><Link to="/graphic" className="menubox" onClick={closeMenu}>Graphic</Link></li>
+            <li className="nav_item"><Link to="/other" className="menubox" onClick={closeMenu}>other</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
