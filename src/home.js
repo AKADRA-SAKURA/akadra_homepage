cd97c6656c917c3d710c5cd9306e0084bdf8e8ab
AKADRA <akadra.sakura7@gmail.com>
2024-02-24 21:03:42 +0900

    ホーム画面原型

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import './box_container.css';
 import './home.css';
 
@@ -20,8 +21,7 @@ function home () {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 2
-             }}
+            transition={{ duration: 2 }}
           >
         <div className="bg">
             <div className="hello">
@@ -30,15 +30,84 @@ function home () {
                         クリエイティブな活動を気ままに</h1>
                 </div>
 
+
                 <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
-                    <div className="photo left"></div>
-                    <div className="photo right"></div>
+                    <div className="photo left">
+                        <div className="text">about</div>
+                    </div>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
                     <div className="overlay">
-                        <div className="text">Your Text Here</div>
-                        <button className="button">Your Button</button>
+                        <Link to="/about" ><button className="button">about</button></Link>
+                        <div className="over_text">AKADRAについて</div>
                     </div>
                 </div>
 
+                <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
+                    <div className="overlay">
+                        <Link to="/web" ><button className="button">Web</button></Link>
+                        <div className="over_text">Programming活動記録</div>
+                    </div>
+                    <div className="photo left">
+                        <div className="text">web</div>
+                    </div>
+                </div>
+
+                <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                    <div className="photo left">
+                        <div className="text">Game</div>
+                    </div>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
+                    <div className="overlay">
+                        <Link to="/game" ><button className="button">Game</button></Link>
+                        <div className="over_text">ゲーム制作記録</div>
+                    </div>
+                </div>
+
+                <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
+                    <div className="overlay">
+                        <Link to="/dtm" ><button className="button">DTM</button></Link>
+                        <div className="over_text">作曲活動記録</div>
+                    </div>
+                    <div className="photo left">
+                        <div className="text">DTM</div>
+                    </div>
+                </div>
+
+                <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                    <div className="photo left">
+                        <div className="text">Graphic</div>
+                    </div>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
+                    <div className="overlay">
+                        <Link to="/graphic" ><button className="button">Graphic</button></Link>
+                        <div className="over_text">おえかき展示場所</div>
+                    </div>
+                </div>
+
+                
+                <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
+                    <div className="overlay">
+                        <Link to="/youtube" ><button className="button">Youtube</button></Link>
+                        <div className="over_text">Youtube・実況活動記録</div>
+                    </div>
+                    <div className="photo left">
+                        <div className="text">youtube</div>
+                    </div>
+                </div>
+
+                <div className={`photo-block ${isHovered ? 'hovered' : ''}`} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                    <div className="photo left">
+                        <div className="text">Other</div>
+                    </div>
+                    <img className="photo right" src={`${process.env.PUBLIC_URL}/akadra1.jpg`} alt="akadraサムネ" onselectstart="return false;" onmousedown="return false;"/>  
+                    <div className="overlay">
+                        <Link to="/other" ><button className="button">Other</button></Link>
+                        <div className="over_text">その他の活動<br />ダンスなど</div>
+                    </div>
+                </div>
             </div>
         </div>
         </motion.div>
