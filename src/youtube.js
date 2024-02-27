98b105c3686ce31492637fc27fb49b061c47315a
AKADRA <akadra.sakura7@gmail.com>
2024-02-27 21:21:27 +0900

    youtube修正とホームにXとブログ

--- src/youtube.js
+++ src/youtube.js
@@ -33,8 +33,8 @@ function youtube(){
             <div className="hitokoto">
               <h1>最新動画</h1>
             </div>
-            <div className='y_box'>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/oNn4cPeH5ls?si=BS5KkDo4aQDjhbSs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div className='y_box_main'>
+              <iframe width='100%' height='auto' src="https://www.youtube.com/embed/oNn4cPeH5ls?si=BS5KkDo4aQDjhbSs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
             <div className="hitokoto">
               <h1>再生リスト（抜粋）</h1>
@@ -46,12 +46,12 @@ function youtube(){
                 <h4>かつて20年近く前に100均で売られていたPCゲームを掘り起こして遊ぶシリーズ</h4>
               </div>
               <div className='y_migi'>         
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=YYgIhHb76hhJc5Dn&amp;list=PLfA6ME9FVmQKDoyI3mnw67-D4dCpjsyG7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width='100%' height='auto' src="https://www.youtube.com/embed/videoseries?si=YYgIhHb76hhJc5Dn&amp;list=PLfA6ME9FVmQKDoyI3mnw67-D4dCpjsyG7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div>
             </div>
             <div className='y_box'>
               <div className='y_migi'>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=64tyUAqUIW6TUiJl&amp;list=PLfA6ME9FVmQLAp2qcgUvuRPJStLx-hQnR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width='100%' height='auto' src="https://www.youtube.com/embed/videoseries?si=64tyUAqUIW6TUiJl&amp;list=PLfA6ME9FVmQLAp2qcgUvuRPJStLx-hQnR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div>
               <div className='y_hidari'>         
                 <h2>ポケットモンスターバイオレット</h2>
@@ -66,7 +66,7 @@ function youtube(){
                 <h4>言わずとしれたドキドキしちゃうアレ</h4>
               </div>
               <div className='y_migi'>         
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/iqZXvBUZ3vc?si=tNI5dkwbClmdh39p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe width='100%' height='auto' src="https://www.youtube.com/embed/iqZXvBUZ3vc?si=tNI5dkwbClmdh39p" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div>
             </div>                      
           </div>
